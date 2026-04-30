5714ff7c5ce2cd8f125dba6e3ee49dbd2c7388cf
javaku9527 <smog3719@gmail.com>
2026-04-30 18:24:44 +0800

    init: leaderboard and session history

---
+++ data.js
@@ -0,0 +1,24 @@
+const PLAYERS = ["Java", "Sky", "軒"];
+
+const MATCH_TYPES = {
+  character: "角色賽",
+  team:      "團體賽",
+  solo:      "單挑賽",
+  random:    "問號賽",
+  sword:     "劍士賽"
+};
+
+// 新增場次：複製下面這個格式，填入日期和各賽制冠軍名字
+// 沒玩的賽制填 null
+const sessions = [
+  // {
+  //   date: "2026-04-30",
+  //   results: {
+  //     character: "Java",
+  //     team:      "Sky",
+  //     solo:      "Benny",
+  //     random:    "Java",
+  //     sword:     null
+  //   }
+  // }
+];
